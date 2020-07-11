28146f80b710097f796d7227f049d852eaaf4313
Yuuwa0519 <30518347+Yuuwa0519@users.noreply.github.com>
2020-07-11 19:07:02 +0900

    PlrChase

--- src/Server/Services/HumService.lua
+++ src/Server/Services/HumService.lua
@@ -1,10 +1,12 @@
 -- Hum Service
--- Username
+-- Yuuwa0519
 -- July 11, 2020
 
 --Service
 local ServerStorage = game:GetService("ServerStorage")
 local PhysicsService = game:GetService("PhysicsService")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
 local base = ServerStorage.Base
 
@@ -37,8 +39,35 @@ function HumService:Start()
         newHum:Activate()
 
         while (true) do
-            newHum:MoveTo(Vector3.new(math.random(-100,100), math.random(0, 20), math.random(-100,100)),5)
-            wait(5)
+            
+            -- newHum:MoveTo(Vector3.new(math.random(-100,100), math.random(0, 20), math.random(-100,100)),5)
+            -- wait(5)
+
+            local cDist, cPlr = math.huge, nil
+
+            for _, plr in pairs(Players:GetPlayers()) do
+                local char = plr.Character
+
+                if (char) then
+                    local dist = (newHum.Base.Position - char.PrimaryPart.Position).Magnitude
+
+                    if (dist < cDist) then
+                        cDist = dist
+                        cPlr = plr
+                    end
+                end
+            end
+
+            if (cPlr) then
+                local  char = cPlr.Character
+
+                if (char) then
+                    newHum:MoveTo(char.PrimaryPart.Position, 1)
+                    wait(1)
+                end
+            end
+
+            RunService.Heartbeat:Wait()
         end
     end)
 end
